4b87cb2db0302a479c1269180aef32567b4ac011
prasetio utomo <prasetio.toyota@gmail.com>
2022-02-24 11:58:32 +0700

    initial app

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -2,3 +2,5 @@
 export * from './authentication.service';
 export * from './user.service';
 export * from './employee.service';
+export * from  './error.interceptor';
+export * from  './jwt.interceptor';
